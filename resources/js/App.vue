7d961bd14c6edab5c67cbbbbefd57d8e7b5f62cb
yh72167 <yh.19972372@gmail.com>
2021-04-26 23:46:54 +0900

    ブランチ まとめ直し

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header/>
+      <main>
+        <div class="#">
+          <RouterView />
+        </div>
+      </main>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+//コンポーネントから別ファイルなどを読み込み際には元のコンポーネントファイルの位置がルートになる。
+import Header from './layout/components/header/header'
+import Footer from './layout/components/footer/footer'
+
+export default {
+    components: {
+    Header,
+    Footer
+    }
+}
+</script>
